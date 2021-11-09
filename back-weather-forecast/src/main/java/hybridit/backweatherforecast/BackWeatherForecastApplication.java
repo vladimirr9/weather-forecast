960a8cecd41275511f39f45380b47a1b0e3964e7
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-11-09 14:56:21 +0100

    Added initial springboot project

---
+++ back-weather-forecast/src/main/java/hybridit/backweatherforecast/BackWeatherForecastApplication.java
@@ -0,0 +1,13 @@
+package hybridit.backweatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWeatherForecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackWeatherForecastApplication.class, args);
+	}
+
+}
---
+++ back-weather-forecast/src/test/java/hybridit/backweatherforecast/BackWeatherForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package hybridit.backweatherforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWeatherForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
